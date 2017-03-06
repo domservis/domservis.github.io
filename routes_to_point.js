4d3dfa1af06578e264fc56653121154213fa9bad
domservis <Mr-redi@mail.ru>
2017-03-07 02:01:24 +0300

    Update routes_to_point.js

--- routes_to_point.js
+++ routes_to_point.js
@@ -28,12 +28,12 @@ ymaps.ready(function () {
 	iconImageHref: 'https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png',
 	iconImageSize: [64, 64], // размер иконки
 	iconImageOffset: [-32, -64], // позиция иконки
-        iconContent: "<div id='dot'><div style='background: url(https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png);' class='ping'></div></div><br><a href='#' id='mp'>открыть</a>",
+        iconContent: "<div id='dot'><div style='background: url(https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png);' class='ping'></div></div><br><a href='#' id='mp'>открыть</a>"
         
 		
-	},
+	}
     
-                                          {	
+                              ,            {	
 		
 		
 		
