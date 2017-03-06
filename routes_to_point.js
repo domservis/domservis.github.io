60945e7eb8ac189926a8dd5991c1fe6666848007
domservis <Mr-redi@mail.ru>
2017-03-07 01:26:45 +0300

    Update routes_to_point.js

--- routes_to_point.js
+++ routes_to_point.js
@@ -28,10 +28,11 @@ ymaps.ready(function () {
     // Метка для конечной точки маршрута////.
         targetPoint = new ymaps.Placemark(targetCoords, {
 		iconLayout: 'default#imageWithContent',
+		iconImageHref: 'https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png',
 		iconImageSize: [64, 64], // размер иконки
 	iconImageOffset: [-32, -64], // позиция иконки
 	balloonContentSize: [370, 370], // размер нашего кастомного балуна в пикселях
-   iconImageHref: 'https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png', iconContent: " <div id='dot'><div style='background: url(https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png);' class='ping'></div></div><br><a href='#' id='mp'>открыть</a>", balloonContentHeader: "<center>{кнопка#1}:{кнопка#2}</center>",
+    iconContent: "<div id='dot'><div style='background: url(https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png);' class='ping'></div></div><br><a href='#' id='mp'>открыть</a>", balloonContentHeader: "<center>{кнопка#1}:{кнопка#2}</center>",
             balloonLayout: "default#imageWithContent",
 	    balloonImageHref: 'https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png',
 	    balloonContentBody: "<iframe id='fr' src='https://domservis.github.io/b.html'frameborder='0' allowfullscreen></iframe>",
