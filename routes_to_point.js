6417fc79ecef5470168c329377fa5fff2bc3f5ad
domservis <Mr-redi@mail.ru>
2017-03-06 23:18:15 +0300

    Update routes_to_point.js

--- routes_to_point.js
+++ routes_to_point.js
@@ -27,7 +27,7 @@ ymaps.ready(function () {
         targetPoint = new ymaps.Placemark(targetCoords, {iconImageSize: [64, 64], // размер иконки
 	iconImageOffset: [-32, -64], // позиция иконки
 	balloonContentSize: [370, 370], // размер нашего кастомного балуна в пикселях
-   iconImageHref: 'https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png', iconContent: " <div id="dot"><div class="ping"></div></div><br><a href='#' id='mp'>открыть</a>", balloonContentHeader: "<center>{кнопка#1}:{кнопка#2}</center>",
+   iconImageHref: 'https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png', iconContent: " <div id='dot'><div class='ping'></div></div><br><a href='#' id='mp'>открыть</a>", balloonContentHeader: "<center>{кнопка#1}:{кнопка#2}</center>",
             balloonLayout: "default#imageWithContent",
 	    balloonImageHref: 'https://raw.githubusercontent.com/domservis/domservis.github.io/master/images/258.png',
 	    balloonContentBody: "<iframe id='fr' src='https://domservis.github.io/b.html'frameborder='0' allowfullscreen></iframe>",
