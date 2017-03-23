fd26d126ac1c21edb6f7bb0dad34e59129be0e6d
domservis <Mr-redi@mail.ru>
2017-03-23 04:46:30 +0300

    Update b.js

--- js/b.js
+++ js/b.js
@@ -22,18 +22,3 @@ window.ChatraSetup = {
 	   Chatra('setColors', {
     chatBg: 'transparent' // фон
 });
-
-
-
-/*
-    (function(d, w, c) {
-        w.ChatraID = 'jjb3L9DqAfuXf9nMT';
-        var s = d.createElement('script');
-        w[c] = w[c] || function() {
-            (w[c].q = w[c].q || []).push(arguments);
-        };
-        s.async = true;
-        s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
-        + '//call.chatra.io/chatra.js';
-        if (d.head) d.head.appendChild(s);
-    })(document, window, 'Chatra');
